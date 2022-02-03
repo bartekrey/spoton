a95607f1eb499fff06709e61224447e8d2002c53
Bartlomiej Rey <bartlomiej.rey@gmail.com>
2022-02-03 18:53:25 +0200

    I hope it's gonna work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 
-function App() {
+function Circle (props) {
+
+  const [active, toogleActive] = useState(false);
+
+  return (
+    <div key={props.key} onClick={() => toogleActive(!active)} className={`circle ${active ? 'active' : ''}`}>
+    </div>
+  )
+}
+
+
+function Tutorial (props) {
+
+  const [open, tooglePopup] = useState(true);
+
+  return (
+    <div className={`popup ${open ? '' : 'closed'}`}>
+      <div className="popup-top">
+        <h1>Tutorial</h1> <img alt="close" onClick={() => tooglePopup(!open)} src="/close.svg"/>
+      </div>
+      <p>Select your own area by tapping on free spots: a new screen will ask for details</p>
+      <p>Taken spots appear in two colours</p>
+      <span><img alt="Green circle" src="/green.svg"/>Green : free to join</span>
+      <span><img alt="Pink circle" src="/pink.svg"/>Pink: don’t disturb</span>
+
+    </div>
+  )
+}
+
+
+export default function App() {
+
+  const [loggedin, LogIn] = useState (false);
+  const [showTutorial, setShowTutorial] = useState (false);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loggedin ?
+        <div className="Screen">
+          <div className="header">
+            <h1>CoID & CoDe Homebase</h1>
+            <h1 className="user">Imran</h1>
+            <button className="logout" onClick={() => LogIn(!loggedin)}>Log out</button>
+          </div>
+
+          <div className="main">
+            <div className="layout">
+              <CirclesArray />
+            </div>
+            <div className="popUps">
+              { showTutorial ? <Tutorial /> : null }
+            </div>
+          </div>
+
+          <div className="footer">
+            <span>Select your area or check out other areas</span>
+            <img alt="info" onClick={() => setShowTutorial(!showTutorial)} src="/info.svg"/>
+          </div>
+
+        </div>
+      :
+        <div className="Welcome">
+          <img alt="Spoton" src="/LOGO.svg"/>
+          <p>You know best what is spot on! </p>
+          <p className="center-text">Customise your spaces to suit your needs  –  focus alone, invite discussion or join others!</p>
+          <img alt="HSL logo" src="/HSL.png" onClick={() => LogIn(!loggedin)}/>
+          <span>Scan HSL card to Log in</span>
+        </div>}
     </div>
   );
 }
 
-export default App;
+function CirclesArray (props) {
+
+  const keysArray =[0, 1, 2, 3, 4, 5, 6, 7 ,8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41]
+
+  return (
+    <div className="CirclesArray">
+
+  {keysArray.map(key => {
+    return (
+      <Circle id={key} />
+    )
+  })}
+    </div>
+  )
+}
