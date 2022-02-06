3ead18f19ac473fcc1d93bcc0543e619017d2f5c
Bartlomiej Rey <bartlomiej.rey@gmail.com>
2022-02-06 13:20:25 +0200

    Koleczko DOne

--- src/App.js
+++ src/App.js
@@ -1,16 +1,57 @@
 import './App.css';
 import React, { useState } from "react";
 
+let activeCircles = [];
+
 function Circle (props) {
 
   const [active, toogleActive] = useState(false);
 
+  function addToSelected () {
+    if (active === false) {
+      toogleActive(!active);
+      activeCircles.push(props.id);
+      console.log(activeCircles);
+    } else {
+      toogleActive(!active);
+      activeCircles= activeCircles.filter(item => item !== props.id)
+      console.log(activeCircles);
+    }
+
+    if (activeCircles !== []) {
+
+    }
+
+  }
+
   return (
-    <div key={props.key} onClick={() => toogleActive(!active)} className={`circle ${active ? 'active' : ''}`}>
+    <div id={props.id} onClick={() => addToSelected()} className={`circle ${active ? 'active' : ''}`}>
     </div>
   )
 }
 
+function CirclesArray (props) {
+
+  const keysArray =[0, 1, 2, 3, 4, 5, 6, 7 ,8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41]
+
+  return (
+    <div className="CirclesArray">
+
+  {keysArray.map(id => {
+    return (
+      <Circle id={id} />
+    )
+  })}
+    </div>
+  )
+}
+
+function CreateSpot (props) {
+  return (
+    <div className="CreateSpot"><img alt="Done" src="./done.svg" />
+    </div>
+  )
+}
 
 function Tutorial (props) {
 
@@ -48,8 +89,12 @@ export default function App() {
           </div>
 
           <div className="main">
+
             <div className="layout">
               <CirclesArray />
+              <CreateSpot />
+
+
             </div>
             <div className="popUps">
               { showTutorial ? <Tutorial /> : null }
@@ -72,20 +117,4 @@ export default function App() {
         </div>}
     </div>
   );
-}
-
-function CirclesArray (props) {
-
-  const keysArray =[0, 1, 2, 3, 4, 5, 6, 7 ,8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41]
-
-  return (
-    <div className="CirclesArray">
-
-  {keysArray.map(key => {
-    return (
-      <Circle id={key} />
-    )
-  })}
-    </div>
-  )
 }
